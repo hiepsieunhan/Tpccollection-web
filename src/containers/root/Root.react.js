1c35c0cf4f73f377a3023cc9b1c4023d4af02697
superpoor <tuananh3668@gmail.com>
2015-09-23 00:07:29 +0800

    Add test material-ui

--- src/containers/root/Root.react.js
+++ src/containers/root/Root.react.js
@@ -1,10 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import mui, { RaisedButton, TextField } from 'material-ui';
+
+let ThemeManager = new mui.Styles.ThemeManager();
 
 export default class Root extends Component {
+  static childContextTypes = {
+    muiTheme: PropTypes.object
+  }
+
+  getChildContext() {
+    return {
+      muiTheme: ThemeManager.getCurrentTheme()
+    }
+  }
+
   render() {
     const {children} = this.props;
     return (
       <div>
+        <TextField
+          hintText="Hint Text"
+          floatingLabelText="Floating Label Text" />
         <p>This is root Component</p>
         {children}
       </div>
