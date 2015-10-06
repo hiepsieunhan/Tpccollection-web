9d6a7f2180b53b4f4aa5ac958e65cd10cf56fefd
buidohiep <buidohiep@gmail.com>
2015-10-07 03:29:46 +0800

    generalize ItemList

---
+++ src/containers/demo/ItemListDemo.react.js
@@ -0,0 +1,24 @@
+﻿import React, {Component} from 'react';
+import ItemList from '../../components/backgroundInfo/ItemList.react';
+import CardContainer from '../../components/CardContainer.react';
+
+export default class Demo extends Component {
+
+  render() {
+    console.log(ItemList);
+    return (
+      <div style={{width: '60%'}}>
+        <CardContainer>
+          <ItemList ref="ItemList" />
+          <button onClick={this.getData}> Show </button>
+        </CardContainer>
+      </div>
+    );
+  }
+
+  getData = () => {
+    const data = this.refs.ItemList.getData();
+    console.log(data);
+    return data;
+  }
+}
